ebcadef5e8ec8617d5ea5292e96442f8c93bf7c4
kowasa <kowasa88@gmail.com>
2020-09-15 02:08:46 +0000

    user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  
+  
+  get 'signup', to: 'users#new'
+  resources :users
   resources :pictures
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_14_115446) do
+ActiveRecord::Schema.define(version: 2020_09_14_151512) do
 
   create_table "pictures", force: :cascade do |t|
     t.integer "user_id"
@@ -20,6 +20,14 @@ ActiveRecord::Schema.define(version: 2020_09_14_115446) do
     t.datetime "updated_at", null: false
     t.index ["title_id"], name: "index_pictures_on_title_id"
     t.index ["user_id"], name: "index_pictures_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
